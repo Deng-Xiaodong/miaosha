973b4c07c62d8e8d99b9aa88f33114206e5bd926
peadx <1823685163@qq.com>
2023-07-31 21:33:38 +0800

    "ip limit"

--- publish/getone.go
+++ publish/getone.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -39,11 +40,12 @@ func main() {
 	//http服务
 	http.HandleFunc("/getone", func(w http.ResponseWriter, r *http.Request) {
 		//Ip限流
-		redislock.RedisClient.SetNX(r.Host, 0, 10*time.Second)
-		redislock.RedisClient.Incr(r.Host)
-		cnt, _ := redislock.RedisClient.Get(r.Host).Int()
-		if cnt > 5 {
-			rsp, _ := json.Marshal(common.Error{Code: 500, Msg: "请求过于频繁，网络拥堵"})
+		ip := strings.Split(r.RemoteAddr, ":")[0]
+		redislock.RedisClient.SetNX(ip, 0, 15*time.Second)
+		redislock.RedisClient.Incr(ip)
+		cnt, _ := redislock.RedisClient.Get(ip).Int()
+		if cnt > 8 {
+			rsp, _ := json.Marshal(common.Error{Code: 504, Msg: "请求过于频繁，网络拥堵"})
 			_, _ = w.Write(rsp)
 			return
 		}
